4f1e1851eeac527e9b6ec6dec6478021d48890b1
Lukas Werner <lukas.m.werner@outlook.com>
2026-05-02 14:54:17 -0700

    fix: ci

--- src/utils/getSortedCollection.ts
+++ src/utils/getSortedCollection.ts
@@ -11,25 +11,23 @@ type SortableEntry = {
 };
 
 const getSortedCollection = <T extends SortableEntry>(entries: T[]) => {
-  return entries
-    .filter(filterCollection)
-    .sort((a, b) => {
-      const featuredDiff =
-        Number(b.data.featured ?? false) - Number(a.data.featured ?? false);
+  return entries.filter(filterCollection).sort((a, b) => {
+    const featuredDiff =
+      Number(b.data.featured ?? false) - Number(a.data.featured ?? false);
 
-      if (featuredDiff !== 0) return featuredDiff;
+    if (featuredDiff !== 0) return featuredDiff;
 
-      const aTime = new Date(
-        a.data.modDatetime ?? a.data.pubDatetime ?? "1970-01-01"
-      ).getTime();
-      const bTime = new Date(
-        b.data.modDatetime ?? b.data.pubDatetime ?? "1970-01-01"
-      ).getTime();
+    const aTime = new Date(
+      a.data.modDatetime ?? a.data.pubDatetime ?? "1970-01-01"
+    ).getTime();
+    const bTime = new Date(
+      b.data.modDatetime ?? b.data.pubDatetime ?? "1970-01-01"
+    ).getTime();
 
-      if (bTime !== aTime) return bTime - aTime;
+    if (bTime !== aTime) return bTime - aTime;
 
-      return a.data.title.localeCompare(b.data.title);
-    });
+    return a.data.title.localeCompare(b.data.title);
+  });
 };
 
 export default getSortedCollection;
